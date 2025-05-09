8924cc69be4709416d815b21a51c59bcef30a7a7
OlioGlobal <olioclientwebsiteleads@gmail.com>
2025-05-09 13:26:19 +0530

    meta:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,8 +19,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>
+          B2B Lead Generation for Manufacturers | Olio Global Adtech
+        </title>
+        <meta
+          name="description"
+          content="Drive 3X Sales growth with Olio Global Adtech, your expert B2B industrial marketing agency. Specialized in B2B lead generation through Digital marketing services"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/fav.png" />
-        <title>Olio B2B</title>
       </Head>
       <div className="min-h-screen flex flex-col">
         <FormProvider>
