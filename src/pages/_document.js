75de22c35e3b57f6c063a4cc6b66fba2de0b5850
OlioGlobal <olioclientwebsiteleads@gmail.com>
2025-05-29 18:29:18 +0530

    old gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,12 +7,12 @@ export default function Document() {
       <Head>
         <Script id="gtm-head" strategy="afterInteractive">
           {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WVQHDXSB');
-  `}
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T6263GJ');
+          `}
         </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -32,7 +32,7 @@ export default function Document() {
       <body className="antialiased">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WVQHDXSB"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6263GJ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
